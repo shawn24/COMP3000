6b6ca7c1de16f689e00f4a8ce57becbbe1e31031
shawn24 <shawn_yao@hotmail.com>
2014-11-11 18:59:21 -0500

    Update 13.c

--- 13.c
+++ 13.c
@@ -1,46 +1,26 @@
 #include <sys/stat.h>
-
 #include <unistd.h>
-
 #include <stdio.h>
 
 int main(int argc, char *argv[])
 
 {
-
 	struct stat buf;
-
 	if(argc < 2)
-
     {
-
         printf("please input filepath!\n");
-
         return;
-
     }
-
-
 
     if(stat(argv[1],&buf)<0)
-
     {
-
         printf("file not exiting.!\n");
-
         return;
-
     }
-
     printf("%s \n", argv[1]);
-
     printf("file size                  = %d \n", buf.st_size); 
-
     printf("blocksize                  = %d \n", buf.st_blksize);
-
     printf("number of blocks allocated = %d \n", buf.st_blocks);
-
     printf("number of hard link        = %d \n", buf.st_nlink);
-
 }
 
